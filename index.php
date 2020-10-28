76a875304dfcff08b3a96e1248b57ef1a7173658
Ahsajol <62804301+Ahsajol@users.noreply.github.com>
2020-10-28 23:42:37 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php include "inc/header.php";?>
+<?php include "inc/slider.php";?>
+
+<div class="contentsection contemplete clear">
+	<div class="maincontent clear">
+<!-- start Pagination -->
+<?php
+	$per_page = 3;
+	if (isset($_GET["page"])){
+		$page = $_GET["page"];
+	}else{
+		$page = 1;
+	}
+	$start_from = ($page-1)*$per_page;
+?>
+<!-- end Pagination -->
+	<?php
+		$query = "select * from tbl_post limit $start_from, $per_page";
+		$post=$db ->select($query);
+		if($post){
+			while($result=$post->fetch_assoc()){
+	?>
+	
+		<div class="samepost clear">
+			<h2><a href="post.php?id =<?php echo $result['id'];?>"><?php echo $result['title'];?></a></h2>
+			<h4><?php echo $fm->formatDate($result['date']);?> By <a href="#"><?php echo $result['author'];?></a></h4>
+			 <a href="#"><img src="admin/<?php echo $result['image'];?>" alt="post image"/></a>
+			<?php echo $fm->textShorten($result['body']);?>
+			<div class="readmore clear">
+				<a href="post.php?id=<?php echo $result['id'];?>">Read More</a>
+			</div>
+		</div>
+	<?php } ?>
+<!-- end While loop -->
+<!-- start Pagination -->
+<?php
+	$query  = "select * from tbl_post ";
+	$result = $db ->select($query);
+	$totall_rows = mysqli_num_rows($result);
+	$totall_pages = ceil($totall_rows/$per_page);
+	echo "<span class='pagination'><a href ='index.php?page=1'>".'First Page'."</a>";
+	for($i=1;$i<=$totall_pages; $i++){
+		echo "<a href ='index.php?page=".$i."'>".$i."</a>";
+	};
+ ?>
+<?php 
+	echo "<a href ='index.php?page=$totall_pages'>".'Last Page'."</a></span>";
+?>
+<!-- End Pagination -->
+			
+			<?php } else { header("location:404.php");} ?> 
+<!-- end if condition -->
+	</div>
+	
+<?php include "inc/sidebar.php";?>
+<?php include "inc/footer.php";?>
